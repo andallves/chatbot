af3ffe6095d3f30a707077b79da0de678a25b380
andallves <andrealves10a@gmail.com>
2024-01-24 18:48:47 -0300

    fix: reload each input

--- Hello.py
+++ Hello.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 from io import StringIO
-import random
 
 score_per_question = 0
 
@@ -17,6 +16,9 @@ if "perguntas" not in st.session_state:
 if "score" not in st.session_state:
     st.session_state.score = 0.0
 
+if "answer" not in st.session_state:
+    st.session_state.answer = ''
+
 if "clicked" not in st.session_state:
     st.session_state.clicked = False
 
@@ -30,7 +32,7 @@ def main():
     if st.session_state.clicked:
         do_upload_file()
 
-    if do_upload_file():
+    if not st.session_state.perguntas.empty:
         chatbot()
 
 
@@ -59,17 +61,17 @@ def check_answer(correct_answer, input_answer):
 
 def question(question_number, index):
     if question_number >= 1:
+
         global score_per_question
         score_per_question = 100 / question_number
         pergunta = st.session_state.perguntas[index]
-
-        user_input = None
 
         if pergunta["tipo"] == "aberta":
             with st.chat_message("assistant"):
                 st.write(pergunta['texto'])
 
             user_input = st.chat_input("E ai?", key=f"user_input_{index}")
+            st.session_state.answer = user_input
             if user_input:
                 with st.chat_message("user"):
                     st.write(user_input)
@@ -80,44 +82,50 @@ def question(question_number, index):
                 options.append(option)
 
             with st.chat_message("assistant"):
-                user_input = st.radio(pergunta['texto'], options=options, key=[random.randint(1, 1000)], index=None)
-
+                user_input = st.radio(pergunta['texto'], options=options, key=f"user_input_{index}", index=None)
+            st.session_state.answer = user_input
             if user_input:
                 with st.chat_message("user"):
                     st.write(user_input)
 
         else:
             with st.chat_message("assistant"):
-                user_input = st.radio(pergunta['texto'], options=["Verdadeiro", "Falso"], key=[random.randint(1, 1000)],
+                user_input = st.radio(pergunta['texto'], options=["Verdadeiro", "Falso"], key=f"user_input_{index}",
                                       index=None)
+            st.session_state.answer = user_input
             if user_input:
                 with st.chat_message("user"):
                     st.write(user_input)
-        if user_input:
-            is_correct = check_answer(pergunta["resposta_correta"], user_input)
+
+        if st.session_state.answer:
+            is_correct = check_answer(pergunta["resposta_correta"], st.session_state.answer.capitalize())
             if is_correct:
                 st.session_state.score += score_per_question
 
-            st.session_state.respostas.append({pergunta["texto"]: user_input})
-            st.session_state.messages.append({"role": "user", "content": user_input})
+            st.session_state.respostas.append({pergunta["texto"]: st.session_state.answer})
+            st.session_state.messages.append({"role": "user", "content": st.session_state.answer})
             st.session_state.messages.append({"role": "assistant", "content": pergunta["texto"]})
-            question(question_number-1, index+1)
-    else:
+            return question(question_number - 1, index + 1)
+
+    elif question_number == 0:
         st.subheader(f"Sua pontuação é: {st.session_state.score}")
         st.write("Faça o download do seu questionário")
         df = pd.DataFrame(st.session_state.messages)
         json_file = df.to_json(orient="records")
         st.download_button("Clique aqui para baixar", data=json_file, file_name="questionario_respondido.json")
 
+    else:
+        return
+
+
+def clean_chatbot():
+    st.session_state.messages = {}
+
 
 def chatbot():
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    question(len(st.session_state.perguntas), 0)
+    number_of_questions = len(st.session_state.perguntas)
+    question(number_of_questions, 0)
 
 
 if __name__ == '__main__':
     main()
-
